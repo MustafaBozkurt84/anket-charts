95455b04706a6c95f789b18824fdf017e78d9788
Mustafa Bozkurt <mwolf845300@gmail.com>
2021-03-21 00:37:08 +0100

    app added

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 @app.route('/',methods=['GET', 'POST'])
 def index():
 
-    select_box = request.form.getlist("skills")
+    select_box = request.form.getlist("hidden_skills")
     if len(select_box)==0:
         select_box=sorular[0:9]
     my_dict = {"label": [], "AnketeKatılanSayı": [], "CevapYuzdesi": [],"soru":[],"chart":[],"chart_type":[]}
@@ -40,7 +40,7 @@ def index():
              my_dict["chart_type"].append('pie')
          else:
              my_dict["chart_type"].append('doughnut')
-    data=pd.DataFrame.from_dict(my_dict)
+
 
 
 
