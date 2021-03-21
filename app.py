d53e04835779dc84e8bffd725010d61ce1a425f8
Mustafa Bozkurt <mwolf845300@gmail.com>
2021-03-21 17:54:58 +0100

    app

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def login():
     auth = request.authorization
     if auth and auth.password == auth_dict[auth.username]:
             token = jwt.encode({'user': auth.username,"password": auth.password, 'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=15)},app.config['SECRET_KEY'])
-            my_token = {'token': token.decode('UTF-8')}
+            my_token = {'token': token}
 
             return redirect(".?token="+my_token["token"])
 
