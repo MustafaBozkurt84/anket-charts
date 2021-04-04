df8a414e61bcd51a9c432d9ca7315c2952838403
Mustafa Bozkurt <mwolf845300@gmail.com>
2021-04-04 03:03:28 +0200

    app added

--- app.py
+++ app.py
@@ -233,6 +233,10 @@ def login():
 def filtered_chart():
     data.token1 = request.args.get('token')
     data.url = "?token=" + data.token1
+    df_question = pd.read_excel(
+        os.path.join(APP_PATH, "Data", "aug_latest.xlsm"),
+        engine='openpyxl',
+    )
     df_question = pd.read_excel("DataStudy02mail.xlsx", sheet_name="questions")
     df_answers = pd.read_excel("DataStudy02mail.xlsx", sheet_name="Sheet4")
     data.my_dict["question_title"] = df_question["title"].tolist()
